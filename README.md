cbd60156127a9ceeb5c64dd328bbf28175dd41c5
Ronny Reinhold <ronny@bnu.com.br>
2019-06-11 07:56:55 -0300

    [INIT] Whatsapp BOT

---
+++ README.md
@@ -0,0 +1,16 @@
+#Whatsapp Client
+BOT that connects throught Whatsapp Web Browser
+
+**NOTE** I've been testing this application and its working for me, but it's not totally safe.
+Actually, it goes against whatsapp policy then use at your own risk. 
+
+## Instalation
+
+
+###TO-DO
+Implement AI.
+Translate all comments to English
+
+### Known Issues
+Can't start Chromium as root user on Linux, it gives a Sandbox error.
+Here you can find a work around: [https://github.com/GoogleChrome/puppeteer/blob/master/docs/troubleshooting.md] (Puppeteer-Troubleshooting)
---
+++ src/models/Base.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Representa estrutura de dados do whatsapp
+ */
+
+ class Base {
+     constructor(client){
+        /**
+         * Cliente que instanciou o bot
+         */
+        Object.defineProperty(this, 'client', { value: client });
+     }
+
+     _clone(){
+         return Object.assign(Object.create(this), this);
+     }
+
+     _patch(data) { return data; }
+
+     /**
+      * Name - Representa este Model no Whatsapp Web
+      */
+     static get WAppModel(){
+         return this.name;
+     }
+
+     /**
+      * Campos extras para adicionar ao Model Serialization
+      */
+     static get extraFields(){
+         return [];
+     }
+
+ }
+
+ module.exports = Base;
